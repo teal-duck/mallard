1b6bc4f72dec7e9aa16eae6ab87691f891527400
Daniel Marshall <djm@mullenandmullen.com>
2015-11-27 17:52:33 +0000

    Config set up

--- desktop/src/com/superduckinvaders/game/desktop/DesktopLauncher.java
+++ desktop/src/com/superduckinvaders/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.superduckinvaders.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.title = "SUPER DUCK INVADERS! - v0.01";
+		config.foregroundFPS = 60;
 		new LwjglApplication(new Game(), config);
 	}
 }

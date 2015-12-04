c7f9620b24becf5cbd9fc87e2bcae0d440d541a5
al1093 <al1093@york.ac.uk>
2015-12-04 02:56:02 +0000

    Started implementation

--- core/src/com/superduckinvaders/game/entity/Item.java
+++ core/src/com/superduckinvaders/game/entity/Item.java
@@ -1,5 +1,9 @@
 package com.superduckinvaders.game.entity;
 
-public class Item {
+public class Item extends Entity {
+	private int ID;
+	public String name;
+	public float cost;
+	public int damage;
 
 }
--- core/src/com/superduckinvaders/game/entity/Particles.java
+++ core/src/com/superduckinvaders/game/entity/Particles.java
@@ -1,5 +1,5 @@
 package com.superduckinvaders.game.entity;
 
-public class Particles {
+public class Particles extends Entity {
 
 }

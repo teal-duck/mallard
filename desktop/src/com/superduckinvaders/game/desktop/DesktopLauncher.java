0e07318dbfa4dafa2774db25525531ba2a0722ef
Oliver McClellan <om639@york.ac.uk>
2016-01-19 14:56:24 +0000

    Added URL.

--- desktop/src/com/superduckinvaders/game/desktop/DesktopLauncher.java
+++ desktop/src/com/superduckinvaders/game/desktop/DesktopLauncher.java
@@ -1,9 +1,16 @@
+/**
+ * URL for executable: https://drive.google.com/file/d/0B4to7QAfaHDIeEE0TG4tR0dyRk0/view?usp=sharing
+ */
+
 package com.superduckinvaders.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.superduckinvaders.game.DuckGame;
 
+/**
+ * Desktop launcher for Super Duck Invaders.
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();

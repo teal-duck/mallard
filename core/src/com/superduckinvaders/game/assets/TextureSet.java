50004748bd81c5d1febab4b04ea97650949aa0dc
Avinash Dwarapu <d.nash.avi@gmail.com>
2016-01-31 10:58:04 +0000

    Implement FaceDirection enum instead of static ints

--- core/src/com/superduckinvaders/game/assets/TextureSet.java
+++ core/src/com/superduckinvaders/game/assets/TextureSet.java
@@ -3,25 +3,27 @@ package com.superduckinvaders.game.assets;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+import java.util.EnumMap;
+
 /**
  * Represents a standard set of textures for character facing and animation.
  */
 public final class TextureSet {
 
     /**
-     * Constants for which way the character is facing.
+     * Enum for which way the character is facing.
      */
-    public static final int FACING_FRONT = 0, FACING_BACK = 1, FACING_LEFT = 2, FACING_RIGHT = 3;
+    public enum FaceDirection { FRONT, BACK, LEFT, RIGHT }
 
     /**
      * Textures to use when the character isn't moving.
      */
-    private TextureRegion[] idleTextures = new TextureRegion[4];
+    private EnumMap<FaceDirection, TextureRegion> idleTextures = new EnumMap<FaceDirection, TextureRegion>(FaceDirection.class);
 
     /**
      * Animations to use when the character is moving.
      */
-    private Animation[] movementAnimations = new Animation[4];
+    private EnumMap<FaceDirection, Animation> movementAnimations = new EnumMap<FaceDirection, Animation>(FaceDirection.class);
 
     /**
      * Initialises this TextureSet with a single texture used for everything.
@@ -59,15 +61,15 @@ public final class TextureSet {
      */
     public TextureSet(TextureRegion front, TextureRegion back, TextureRegion left, TextureRegion right,
                       Animation movingFront, Animation movingBack, Animation movingLeft, Animation movingRight) {
-        idleTextures[FACING_FRONT] = front;
-        idleTextures[FACING_BACK] = back;
-        idleTextures[FACING_LEFT] = left;
-        idleTextures[FACING_RIGHT] = right;
+        idleTextures.put(FaceDirection.FRONT,  front);
+        idleTextures.put(FaceDirection.BACK,    back);
+        idleTextures.put(FaceDirection.LEFT,  left);
+        idleTextures.put(FaceDirection.RIGHT, right);
 
-        movementAnimations[FACING_FRONT] = movingFront;
-        movementAnimations[FACING_BACK] = movingBack;
-        movementAnimations[FACING_LEFT] = movingLeft;
-        movementAnimations[FACING_RIGHT] = movingRight;
+        movementAnimations.put(FaceDirection.FRONT,  movingFront);
+        movementAnimations.put(FaceDirection.BACK,    movingBack);
+        movementAnimations.put(FaceDirection.LEFT,  movingLeft);
+        movementAnimations.put(FaceDirection.RIGHT, movingRight);
     }
 
     /**
@@ -76,7 +78,7 @@ public final class TextureSet {
      * @return the width
      */
     public float getWidth() {
-        return idleTextures[0].getRegionWidth();
+        return idleTextures.get(FaceDirection.FRONT).getRegionWidth();
     }
 
     /**
@@ -85,7 +87,7 @@ public final class TextureSet {
      * @return the height
      */
     public float getHeight() {
-        return idleTextures[0].getRegionHeight();
+        return idleTextures.get(FaceDirection.FRONT).getRegionHeight();
     }
 
     /**
@@ -95,11 +97,11 @@ public final class TextureSet {
      * @param stateTime the state time
      * @return the appropriate texture
      */
-    public TextureRegion getTexture(int facing, float stateTime) {
+    public TextureRegion getTexture(FaceDirection facing, float stateTime) {
         if (stateTime > 0) {
-            return movementAnimations[facing].getKeyFrame(stateTime, true);
+            return movementAnimations.get(facing).getKeyFrame(stateTime, true);
         } else {
-            return idleTextures[facing];
+            return idleTextures.get(facing);
         }
     }
 }

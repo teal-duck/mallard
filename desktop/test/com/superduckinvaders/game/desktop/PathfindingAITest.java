25e2fd9c7178879346446c43a84f6fa36a86ca92
Avinash Dwarapu <d.nash.avi@gmail.com>
2016-02-16 09:27:33 +0000

    Add test placeholders

---
+++ desktop/test/com/superduckinvaders/game/desktop/PathfindingAITest.java
@@ -0,0 +1,68 @@
+package com.superduckinvaders.game.desktop;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Vector2;
+import com.superduckinvaders.game.DuckGame;
+import com.superduckinvaders.game.Round;
+import com.superduckinvaders.game.ai.PathfindingAI;
+import com.superduckinvaders.game.assets.Assets;
+import com.superduckinvaders.game.assets.TextureSet;
+import com.superduckinvaders.game.desktop.util.LwjglTestRunner;
+import com.superduckinvaders.game.entity.Mob;
+import com.superduckinvaders.game.entity.Player;
+import com.superduckinvaders.game.screen.GameScreen;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test mallard's "unique" implementation of A* search.
+ */
+@RunWith(LwjglTestRunner.class)
+@Ignore // Until we fix the errors!
+public class PathfindingAITest {
+    private Round round;
+    private TextureSet textureSet;
+
+    @Before
+    public void setUp() {
+        TiledMap map = new TiledMap(); // FIXME: Replace with a dummy map or something.
+        round = spy(new Round(mock(DuckGame.class), map));
+        textureSet = mock(TextureSet.class);
+    }
+
+    @Test
+    public void ChasesPlayerHorizontally() {
+        Player player = new Player(round, 0f, 0f);
+        when(round.getPlayer()).thenReturn(player);
+        Mob mob = new Mob(round, 10f, 0f, 100, textureSet, 5, new PathfindingAI(round, 0));
+        round.update(1f);
+        assertEquals(mob.getPosition(), new Vector2(5f, 0f));
+    }
+
+    @Test
+    public void ChasesPlayerVertically() {
+        Player player = new Player(round, 0f, 0f);
+        when(round.getPlayer()).thenReturn(player);
+        Mob mob = new Mob(round, 0f, 10f, 100, textureSet, 5, new PathfindingAI(round, 0));
+        round.update(1f);
+        assertEquals(mob.getPosition(), new Vector2(0f, 5f));
+    }
+
+    @Test
+    public void StopsIfRangeProvided() {
+        Player player = new Player(round, 0f, 0f);
+        when(round.getPlayer()).thenReturn(player);
+        Mob mob = new Mob(round, 0f, 10f, 100, textureSet, 5, new PathfindingAI(round, 2));
+        round.update(10f);
+        assertEquals(mob.getPosition(), new Vector2(0f, 2f)); // Stops at 2y from player.
+    }
+
+    @Test
+    public void TakesExpectedRouteAroundObstacle() {
+    }
+}
---
+++ desktop/test/com/superduckinvaders/game/desktop/ProjectileTest.java
@@ -0,0 +1,44 @@
+package com.superduckinvaders.game.desktop;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Vector2;
+import com.superduckinvaders.game.DuckGame;
+import com.superduckinvaders.game.Round;
+import com.superduckinvaders.game.ai.PathfindingAI;
+import com.superduckinvaders.game.assets.Assets;
+import com.superduckinvaders.game.assets.TextureSet;
+import com.superduckinvaders.game.desktop.util.LwjglTestRunner;
+import com.superduckinvaders.game.entity.Mob;
+import com.superduckinvaders.game.entity.PhysicsEntity;
+import com.superduckinvaders.game.entity.Player;
+import com.superduckinvaders.game.entity.Projectile;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test mallard's "unique" implementation of A* search.
+ */
+@RunWith(LwjglTestRunner.class)
+@Ignore // Issues with asset loading :(
+public class ProjectileTest {
+    private Round round;
+    private PhysicsEntity owner;
+
+    @Before
+    public void setUp() {
+        Assets.load();
+        round = mock(Round.class);
+        owner = mock(PhysicsEntity.class);
+    }
+
+    @Test
+    public void StartsAtCorrectPosition() {
+        Projectile proj = new Projectile(round, new Vector2(100, 200), new Vector2(10, 20), 0, owner);
+        assertEquals(proj.getPosition(), new Vector2(100, 200));
+    }
+}
